e94102b8445ae6807acd2afb6b36bfa47b845767
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-10-27 22:22:33 +0530

    fix errorHandler middleware error

--- src/middleware/errorHandler.middleware.js
+++ src/middleware/errorHandler.middleware.js
@@ -9,7 +9,7 @@ export const errorHandler = (err, req, res, next) => {
         })
     }
 
-    return res.status(err.statusCode).json({
+    return res.status(500).json({
         success: false,
         message: "Internal server error!",
         errors: []
